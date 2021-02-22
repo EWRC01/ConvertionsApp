7aa6cabdd79f0ddc691f4b2e5858ce3efe59cf86
@Wilfredo <edwin1026@hotmail.es>
2021-02-21 21:37:50 -0600

    Implementacion de pages restantes

--- src/components/index.vue
+++ src/components/index.vue
@@ -9,18 +9,44 @@
       <!--Implementacion de la primera pagina-->
       <div class="page1Lengths">
         <Page1/>
+        
         </div>
+        <!--Implementacion de la segunda pagina-->
+        <div class="page2Lengths">
+          <Page2/>
+          </div>
+          <!--Implementacion de la tercera pagina-->
+          <div class="page3">
+              <Page3/>
+            </div>
+            <!--Implementacion de la cuarta pagina-->
+            <div class="page4">
+              <Page4/>
+              </div>
+              <!--Implementacion de la quinta pagina-->
+              <div class="page5">
+              <Page5/>
+              </div>
   </section>
  
 </template>
 
 <script lang="js">
 import Page1 from './pageLengths.vue';
+import Page2 from './pageLengths1.vue';
+import Page3 from './pageLengths2.vue';
+import Page4 from './pageLengths3.vue';
+import Page5 from './pageLengths4.vue';
+
 
   export default  {
     name: 'src-components-index',
     components:{
-      Page1
+      Page1,
+      Page2,
+      Page3,
+      Page4,
+      Page5
 
     },
     props: [],
--- src/components/pageLengths.vue
+++ src/components/pageLengths.vue
@@ -23,19 +23,16 @@
         label-align-sm="right"
         class="mb-0"
       >
-        <b-form-group label="Por favor seleccione una opcion!">
-          <b-form-radio-group
-          id="radio-group-2"
-          v-model="selected"
-          name="radio-sub-component"
-          >
-          <b-form-radio id="kmToM"  value="1">Kilometros a Metros</b-form-radio>
-           <b-form-radio id="mTokKm"  value="1">Metros a Kilometros</b-form-radio>
-            <b-form-radio id="mToCm"  value="1">Metros a Centimetros</b-form-radio>
-             <b-form-radio id="cmToM"  value="1">Centimetros a Metros</b-form-radio>
-              <b-form-radio id="mToF"  value="1">Metros a pies</b-form-radio>
-          </b-form-radio-group>
-        </b-form-group>
+        <b-form-select v-model="selected">
+          
+          <b-form-select-option :value="null">Por favor seleccione una opcion</b-form-select-option>
+          <b-form-select-option   value="a">Kilometros a Metros</b-form-select-option>
+           <b-form-select-option id="opB"  value="b">Metros a Kilometros</b-form-select-option>
+            <b-form-select-option id="opC"  value="c">Metros a Centimetros</b-form-select-option>
+             <b-form-select-option id="opD"  value="d">Centimetros a Metros</b-form-select-option>
+              <b-form-select-option id="opE"  value="e">Metros a pies</b-form-select-option>
+              
+        </b-form-select>
         
         <hr>
         <b-button v-on:click="calcular1" variant="dark" size="lg" class="pt-2">Convertir a</b-button>
@@ -56,7 +53,7 @@
     },
     data () {
       return {
-        selected: '',
+        selected: null,
         data1:''
         
        
@@ -67,13 +64,9 @@
     },
     methods: {
       calcular1(){
-       var elemento = document.getElementById("kmToM");
-       var elemento1= document.getElementById("mTokKm");
-       var elemento2 = document.getElementById("mToCm");
-       var elemento3 = document.getElementById("cmToM");
-       var elemento4 = document.getElementById("mToF");
+     var referencia = this.selected;
        
-        if (elemento.checked == true) {
+        if (referencia == 'a') {
          
           
           var obtenern= this.data1
@@ -87,7 +80,7 @@
          
 
         }
-        else if (elemento1.checked == true)
+        else if (referencia == 'b')
         {
           var obtenern1=this.data1
           var equivalencia1 = 1000;
@@ -97,7 +90,7 @@
          
 
         }
-        else if (elemento2.checked == true)
+        else if (referencia == 'c')
         {
           var obtenern2=this.data1
           var equivalencia2 = 100;
@@ -106,7 +99,7 @@
           alert(operacion2 + " Centimetros");
             
         }
-        else if (elemento3.checked == true)
+        else if (referencia == 'd')
         {
           var obtenern3=this.data1
           var equivalencia3 = 100;
@@ -116,7 +109,7 @@
          
 
         }
-        else if (elemento4.checked == true)
+        else if (referencia == 'e')
         {
           var obtenern4=this.data1
           var equivalencia4 = 3.281;
